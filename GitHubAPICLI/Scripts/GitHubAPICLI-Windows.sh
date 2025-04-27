3fb9d81f328d1d2853868a824066a6fd5d0250b6
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2025-04-27 18:48:36 -0400

    Updated Scripts for customizability

--- GitHubAPICLI/Scripts/GitHubAPICLI-Windows.sh
+++ GitHubAPICLI/Scripts/GitHubAPICLI-Windows.sh
@@ -4,6 +4,8 @@
 GitHubPAT=""
 Secret=""
 DefaultImage=""
+DockerImage="ghcr.io/nano-dna-studios/githubapicli-server:latest"
+OutputPort=8080
 
 # Run the Container
-docker run --name githubapicli-webhookserver -e GitHubPAT=$GitHubPAT -e Secret=$Secret -e DefaultImage=$DefaultImage --privileged -e DOCKER_HOST=tcp://host.docker.internal:2375 -p 8080:8080 dotnet-idk
+docker run --name githubapicli-webhookserver -e GitHubPAT=$GitHubPAT -e Secret=$Secret -e DefaultImage=$DefaultImage --privileged -e DOCKER_HOST=tcp://host.docker.internal:2375 -p $OutputPort:8080 $DockerImage
